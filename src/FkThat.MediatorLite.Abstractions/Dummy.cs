e5531c34798e7239fb5f780fbf1ae809b9fd195c
FkThat <fkthat@yandex.ru>
2021-04-14 08:54:46 +0300

    Add projects

---
+++ src/FkThat.MediatorLite.Abstractions/Dummy.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FkThat.MediatorLite.Abstractions
+{
+    /// <summary>
+    /// Dummy class
+    /// </summary>
+    public class Dummy
+    {
+    }
+}
---
+++ src/FkThat.MediatorLite/Dummy.cs
@@ -0,0 +1,14 @@
+namespace FkThat.MediatorLite
+{
+    /// <summary>
+    /// Dummy class
+    /// </summary>
+    public class Dummy
+    {
+        /// <summary>
+        /// Gets the foo.
+        /// </summary>
+        /// <value>The foo.</value>
+        public string Foo => "Hello";
+    }
+}
