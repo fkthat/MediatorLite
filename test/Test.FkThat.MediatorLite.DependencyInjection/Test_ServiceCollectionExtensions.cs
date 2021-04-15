c037bb4c7c2f5dba214a586662c84e47b313a723
FkThat <fkthat@yandex.ru>
2021-04-16 01:46:00 +0300

    Mediator registration test.

--- test/Test.FkThat.MediatorLite.DependencyInjection/Test_ServiceCollectionExtensions.cs
+++ test/Test.FkThat.MediatorLite.DependencyInjection/Test_ServiceCollectionExtensions.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Linq;
+using System.Threading.Tasks;
 using FakeItEasy;
 using FkThat.MediatorLite;
 using FluentAssertions;
@@ -29,6 +31,39 @@ namespace Microsoft.Extensions.DependencyInjection
                 d.ServiceType == typeof(IMediator) &&
                 d.ImplementationType == typeof(Mediator) &&
                 d.Lifetime == ServiceLifetime.Transient);
+        }
+
+        [Fact]
+        public void AddMediator_ShouldRegisterAutoTypes()
+        {
+            ServiceCollection services = new();
+            services.AddTransient<Handler>();
+            services.AddMediator();
+
+            services.Should().Contain(d =>
+                d.ServiceType == typeof(IMediatorConfiguration) &&
+                d.Lifetime == ServiceLifetime.Singleton);
+
+            services.Should().Contain(d =>
+                d.ServiceType == typeof(IMediator) &&
+                d.ImplementationType == typeof(Mediator) &&
+                d.Lifetime == ServiceLifetime.Transient);
+
+            var configuration = services
+                .Where(sd => sd.ServiceType == typeof(IMediatorConfiguration))
+                .Select(sd => sd.ImplementationInstance)
+                .Cast<IMediatorConfiguration>()
+                .First();
+
+            configuration.MessageHandlers.Should()
+                .Contain((typeof(Message), typeof(Handler)));
+        }
+
+        public class Message { }
+
+        public class Handler : IMessageHandler<Message>
+        {
+            public Task HandleMessageAsync(Message message) => throw new NotImplementedException();
         }
     }
 }
