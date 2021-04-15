d8ea3a3cc28c4e616be1c64f2f4374576f133458
FkThat <fkthat@yandex.ru>
2021-04-15 14:01:02 +0300

    Refactor Test_Mediator to mocks

--- test/Test.FkThat.MediatorLite/Test_Mediator.cs
+++ test/Test.FkThat.MediatorLite/Test_Mediator.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using FakeItEasy;
 using Xunit;
@@ -12,17 +13,34 @@ namespace FkThat.MediatorLite
         {
             var h1 = A.Fake<IMessageHandler<Message1>>();
             var h2 = A.Fake<IMessageHandler<Message2>>();
+            var configuration = A.Fake<IMediatorConfiguration>();
+
+            // arrange message handlers
+            A.CallTo(() => configuration.MessageHandlers).Returns(new[] {
+                (typeof(Message1), h1.GetType()),
+                (typeof(Message2), h2.GetType()),
+            });
+
+            // arrange message dispatchers
+            A.CallTo(() => configuration.MessageDispatchers).Returns(
+                new Dictionary<Type, Func<object, object, Task>> {
+                    [typeof(Message1)] = (h, m) =>
+                        ((IMessageHandler<Message1>)h).HandleMessageAsync((Message1)m),
+                    [typeof(Message2)] = (h, m) =>
+                        ((IMessageHandler<Message2>)h).HandleMessageAsync((Message2)m),
+                });
+
             var serviceProvider = A.Fake<IServiceProvider>();
             A.CallTo(() => serviceProvider.GetService(h1.GetType())).Returns(h1);
             A.CallTo(() => serviceProvider.GetService(h2.GetType())).Returns(h2);
-            MediatorConfiguration configuration = new();
-            configuration.AddHandler(h1.GetType());
-            configuration.AddHandler(h2.GetType());
+
             Mediator sut = new(serviceProvider, configuration);
             Message1 msg1 = new();
             Message2 msg2 = new();
             await sut.SendMessageAsync(msg1);
             await sut.SendMessageAsync(msg2);
+
+            // verify messages dispatched
             A.CallTo(() => h1.HandleMessageAsync(msg1)).MustHaveHappened();
             A.CallTo(() => h2.HandleMessageAsync(msg2)).MustHaveHappened();
         }
